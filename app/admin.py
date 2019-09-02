5d73332cec260da81ab80f17d1c62bd39add2f09
Risper Akinyi <email@example.com>
2019-09-02 10:01:19 +0300

    Register the model classes in the admin.py

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from .models import Profiles,Images,Comments
 from django.contrib import admin
 
-# Register your models here.
+# Register your model class here.
 admin.site.register(Profiles)
 admin.site.register(Images)
 admin.site.register(Comments)
