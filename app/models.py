3bc1f087a7bfd098643f5ab6f3239be7f39fc30c
Risper Akinyi <email@example.com>
2019-09-02 09:55:28 +0300

    Add comments  model in app/model

--- app/models.py
+++ app/models.py
@@ -58,5 +58,21 @@ class Images(models.Model):
         images = Images.objects.all()
         return images
     
+class Comments(models.Model):
+    comment = models.CharField(max_length=100)
+    posted = models.DateTimeField(auto_now=True)
+    image = models.ForeignKey(Images,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    
+    def save_comment(self):
+        self.save()
+    
+    def delete_comments(self):
+        self.delete()
+        
+    @classmethod
+    def get_comment_by_image(cls,id):
+        comment = Comments.objects.filter(image__pk = id)
+        return comment
     
     
