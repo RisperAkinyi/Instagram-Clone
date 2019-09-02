7a4b093dd5c4688410a3d776906d07e867a4f80e
Risper Akinyi <email@example.com>
2019-09-02 09:53:04 +0300

    Add the profile model in app/model

--- app/models.py
+++ app/models.py
@@ -3,6 +3,32 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
 
+class Profiles(models.Model):
+    image = models.ImageField(blank=True, )
+    bio = models.CharField(max_length=100)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+        
+    @classmethod
+    def get_profile_by_name(cls, name):
+        profile = Profiles.objects.filter(user__username__icontains = name)
+        return profile
+    
+    @classmethod
+    def filter_profile_by_id(cls, id):
+        profile = Profiles.objects.filter(user = id).first()
+        return profile
+    
+    @classmethod
+    def get_profile_by_id(cls,id):
+        profile = Profiles.objects.get(user = id)
+        return profile
+    
 
 class Comments(models.Model):
     comment = models.CharField(max_length=100)
