b84048f27462b074cbee73a64d56192199b8c3dc
Risper Akinyi <email@example.com>
2019-09-02 10:03:52 +0300

    add the index.html view function in the views.py

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from .models import Profiles,Images,Comments
 from django.contrib import admin
 
-# Register your model class here.
+# Register your models here.
 admin.site.register(Profiles)
 admin.site.register(Images)
 admin.site.register(Comments)
--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import PostComment,PostImageForm,PostProfile
 from .models import Images, Profiles, Comments 
 
-# Create your views here.
+# Create your view function here.
 @login_required(login_url='/accounts/register/')
 def index(request):
     images = Images.get_all_images()
