56e219723cbe0061d8858c8fc42cbb3097a8e729
Risper Akinyi <email@example.com>
2019-09-02 09:54:26 +0300

    Add the image model in app/model

--- app/models.py
+++ app/models.py
@@ -29,22 +29,34 @@ class Profiles(models.Model):
         profile = Profiles.objects.get(user = id)
         return profile
     
-
-class Comments(models.Model):
-    comment = models.CharField(max_length=100)
+class Images(models.Model):
+    image = models.ImageField(blank=True, )
+    caption = models.CharField(max_length=100)
     posted = models.DateTimeField(auto_now=True)
-    image = models.ForeignKey(Images,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile = models.ForeignKey(User,on_delete=models.CASCADE)
     
-    def save_comment(self):
+    class Meta:
+        ordering = ('-posted',)
+        
+    def save_image(self):
         self.save()
     
-    def delete_comments(self):
+    def delete_image(self):
         self.delete()
-        
+    @classmethod  
+    def get_image_by_id(cls,id):
+        image = Images.objects.get(pk=id)
+        return image
+    
     @classmethod
-    def get_comment_by_image(cls,id):
-        comment = Comments.objects.filter(image__pk = id)
-        return comment
+    def get_profile_images(cls,profile):
+        images = Images.objects.filter(profile__pk= profile)
+        return images
+    
+    @classmethod
+    def get_all_images(cls):
+        images = Images.objects.all()
+        return images
+    
     
     
