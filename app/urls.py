05d173177f93c88cb3ddf68b42b49c3703d45e09
Risper Akinyi <email@example.com>
2019-09-02 10:14:10 +0300

    add the base.html in templates/registration

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-#url
+
 urlpatterns = [
     url(r'^$',views.index,name='home'),
     url(r'^user/(?P<username>\w+)',views.profile,name='profile'),
